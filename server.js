73ee2a0ee0ba4d158da5cb3e9c7e4565d92b17b3
Lemur <piotr.libucha.off@gmail.com>
2023-12-09 20:24:19 +0100

    Final changes before deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const port = process.env.PORT || 5000
 
 app.use(express.json());
@@ -8,6 +9,14 @@ app.use('/user', require('./src/routes/user'));
 app.use('/balance', require('./src/routes/balance'));
 app.use('/transaction', require('./src/routes/transaction'));
 app.use('/token', require('./src/routes/token'));
+
+
+app.use(express.static(path.join(__dirname, 'cloud_project/build')));
+if(process.env.NODE_ENV === 'production')
+{ 
+    app.use(express.static(path.join(__dirname, 'cloud_project/build')));
+    app.get('*', (req, res) => {res.sendFile(path.join(__dirname = 'cloud_project/build/index.html'));  })
+}
 
 app.listen(port, () => {
     console.log(`Success! Your application is running on port ${port}.`);
